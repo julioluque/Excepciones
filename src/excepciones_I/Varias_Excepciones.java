6e2dd00febdc14d93aa3cd4c6a986fd1c131358f
Julio Luque <julio.a.luque@gmail.com>
2020-04-14 18:51:05 -0300

    Review

--- src/excepciones_I/Varias_Excepciones.java
+++ src/excepciones_I/Varias_Excepciones.java
@@ -10,15 +10,14 @@ public class Varias_Excepciones {
 			division();
 		} catch (ArithmeticException e) {
 			System.out.println("1 . No se permite la division por cero");
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-			System.out.println("4 . Se genero un error del tipo: "+ e.getClass().getName());
-			e.printStackTrace();
+			System.out.println("2 . Mensaje de throwable:" + e.getMessage());
+			System.out.println("3 . Se genero un error del tipo: "+ e.getClass().getName());
+			System.out.print("4 . "); e.printStackTrace();
 		} catch (NumberFormatException e) {
 			System.out.println("1 . No se permiten letras");
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-			System.out.println("4 . Se genero un error del tipo: "+ e.getClass().getName());
+			System.out.println("2 . Mensaje de throwable:" + e.getMessage());
+			System.out.println("3 . Se genero un error del tipo: "+ e.getClass().getName());
+			System.out.print("4 . "); e.printStackTrace();
 		}
 
 	}
